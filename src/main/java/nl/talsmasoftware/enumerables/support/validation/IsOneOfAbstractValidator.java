3bfc5713fe0a53836f32c79023b2113937d977ac
Sjoerd Talsma <talsma.s@tkppensioen.nl>
2016-06-06 16:46:03 +0200

    Started work on javax.validation constraints.

---
+++ src/main/java/nl/talsmasoftware/enumerables/support/validation/IsOneOfAbstractValidator.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2016 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.talsmasoftware.enumerables.support.validation;
+
+import nl.talsmasoftware.enumerables.constraints.IsOneOf;
+
+import javax.validation.ConstraintValidator;
+import java.util.Locale;
+
+import static java.util.Arrays.binarySearch;
+import static java.util.Arrays.sort;
+
+/**
+ * @author <a href="mailto:info@talsma-software.nl">Sjoerd Talsma</a>
+ */
+abstract class IsOneOfAbstractValidator<TYPE> implements ConstraintValidator<IsOneOf, TYPE> {
+
+    private String[] acceptedSortedValues = null;
+    private boolean caseSensitive = true;
+
+    @IsOneOf
+    public void initialize(IsOneOf constraintAnnotation) {
+        acceptedSortedValues = constraintAnnotation.value().clone();
+        caseSensitive = constraintAnnotation.caseSensitive();
+        if (!caseSensitive) for (int i = 0; i < acceptedSortedValues.length; i++) {
+            acceptedSortedValues[i] = lowercase(acceptedSortedValues[i]);
+        }
+        sort(acceptedSortedValues);
+    }
+
+    private static String lowercase(String value) {
+        return value.toLowerCase(Locale.ENGLISH);
+    }
+
+    protected boolean isAccepted(String value) {
+        return value == null || binarySearch(acceptedSortedValues, caseSensitive ? value : lowercase(value)) >= 0;
+    }
+
+}
