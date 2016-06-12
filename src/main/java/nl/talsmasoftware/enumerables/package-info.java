7d7c3dfd609c232593e8d5e1f2436e1a9c547c28
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2016-06-12 13:14:41 +0200

    Added some package javadoc.

---
+++ src/main/java/nl/talsmasoftware/enumerables/package-info.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ *
+ */
+
+/**
+ * The main class in this library is
+ * {@link nl.talsmasoftware.enumerables.Enumerable Enumerable}, similar to the standard Java {@link java.lang.Enum Enum}
+ * type. It has one special feature that makes it more suitable for using in an API that you need to maintain.
+ * <p>
+ * Ever have an actual {@link Enum} in an exposed API? Then have the customer come up with an additional value for
+ * that {@link Enum} that should also be supported?
+ * <strong>*Bang!*</strong> there goes your API compatibility. You will either have to tel all your existing customers
+ * <em>"sorry, the api is now broken"</em> or create a new version <strong>beside</strong> the existing API and declare
+ * the old one deprecated. However, you'll still have to think about how to represent the additional value in the old
+ * version or create a special exception for this case.<br>
+ * Nasty in any case.
+ * <p>
+ * That is exactly the reason we've created the <code>Enumerable</code> type. It is similar to {@link Enum} in that it
+ * represents a number of <em>known constants</em> but also offers a possibility to represent
+ * <em>yet unknown additional values</em> by parsing them. In fact; parsing a known value results in the single constant
+ * reference. Same goes for serialization and deserialization.
+ * <p>
+ * Class diagram for this package:<br><center><img src="package.svg" alt="package class diagram"></center>
+ *
+ * @author <a href="mailto:info@talsma-software.nl">Sjoerd Talsma</a>
+ */
+package nl.talsmasoftware.enumerables;
