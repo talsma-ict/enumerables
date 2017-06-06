4afcf8e3b85b397bdb7e9e01c7327bcc5188cce1
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-06-06 15:27:21 +0200

    Add provider unit-test.

---
+++ enumerables-jaxrs/src/test/java/nl/talsmasoftware/enumerables/jaxrs/EnumerableParamConverterProviderTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016-2017 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.enumerables.jaxrs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * @author Sjoerd Talsma
+ */
+public class EnumerableParamConverterProviderTest {
+
+    EnumerableParamConverterProvider provider;
+
+    @Before
+    public void setup() {
+        provider = new EnumerableParamConverterProvider();
+    }
+
+    @Test
+    public void testNonEnumerableTypes() {
+        assertThat(provider.getConverter(null, null, null), is(nullValue()));
+        assertThat(provider.getConverter(String.class, null, null), is(nullValue()));
+    }
+
+    @Test
+    public void testProvideEnumerableParamConverter() {
+        assertThat(provider.getConverter(TestEnumerable.class, null, null),
+                is(instanceOf(EnumerableParamConverter.class)));
+        assertThat(provider.getConverter(TestEnumerable.class, null, null).fromString("2nd"),
+                is(sameInstance(TestEnumerable.SECOND)));
+    }
+
+}
