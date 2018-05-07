d268655f2ff6b717eb3dc672d6928493f949b1d1
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-05-07 13:45:05 +0200

    Implement JDBI3 ArgumentFactory and ColumnMapperFactory

---
+++ enumerables-jdbi3/src/main/java/nl/talsmasoftware/enumerables/jdbi3/EnumerableArgumentFactory.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016-2018 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.enumerables.jdbi3;
+
+import nl.talsmasoftware.enumerables.Enumerable;
+import org.jdbi.v3.core.argument.Argument;
+import org.jdbi.v3.core.argument.ArgumentFactory;
+import org.jdbi.v3.core.config.ConfigRegistry;
+
+import java.lang.reflect.Type;
+import java.util.Optional;
+
+/**
+ * {@link ArgumentFactory JDBI ArgumentFactory} for {@link Enumerable} objects to bind {@code Enumerable} objects
+ * as JDBI arguments for {@code String} parameters.
+ * <p>
+ * This {@link ArgumentFactory} can be registered similar to the following example:
+ * <pre>
+ *     {@literal @}RegisterArgumentFactory(EnumerableArgumentFactory.class)
+ *     {@literal @}UseRowMapper(MyValueObjectMapper.class) // defining a custom RowMapper
+ *      public interface MyDao {
+ *          ...
+ *         {@literal @}SqlQuery("select ... as obj from ...  where type = :type")
+ *          List&lt;MyValueObject> queryWithType({@literal @}Bind("type") MyTypeEnumerable type);
+ *          ...
+ *      }
+ * </pre>
+ *
+ * @author Sjoerd Talsma
+ */
+public class EnumerableArgumentFactory implements ArgumentFactory {
+
+    public Optional<Argument> build(Type type, Object value, ConfigRegistry config) {
+        return Optional.ofNullable(value)
+                .filter(Enumerable.class::isInstance).map(Enumerable.class::cast)
+                .map(enumerable -> (paramIdx, stmt, ctx) -> stmt.setString(paramIdx, Enumerable.print(enumerable)));
+    }
+
+}
---
+++ enumerables-jdbi3/src/main/java/nl/talsmasoftware/enumerables/jdbi3/EnumerableColumnMapperFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016-2018 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.enumerables.jdbi3;
+
+import nl.talsmasoftware.enumerables.Enumerable;
+import org.jdbi.v3.core.config.ConfigRegistry;
+import org.jdbi.v3.core.generic.GenericTypes;
+import org.jdbi.v3.core.mapper.ColumnMapper;
+import org.jdbi.v3.core.mapper.ColumnMapperFactory;
+
+import java.lang.reflect.Type;
+import java.util.Optional;
+
+/**
+ * {@link ColumnMapperFactory JDBI ColumnMapperFactory} for {@link Enumerable} subtypes.
+ *
+ * @author Sjoerd Talsma
+ */
+public class EnumerableColumnMapperFactory implements ColumnMapperFactory {
+
+    @Override
+    public Optional<ColumnMapper<?>> build(Type type, ConfigRegistry config) {
+        return enumerableType(type)
+                .map(enumerableType -> (rs, colNr, ctx) -> Enumerable.parse(enumerableType, rs.getString(colNr)));
+    }
+
+    @SuppressWarnings("unchecked") // Actually checked by isAssignableFrom
+    private static Optional<Class<? extends Enumerable>> enumerableType(Type type) {
+        Optional<Class<?>> erasedType = Optional.ofNullable(type).map(GenericTypes::getErasedType);
+        return erasedType.filter(Enumerable.class::isAssignableFrom).map(clz -> (Class<? extends Enumerable>) clz);
+    }
+
+}
